d310c197f22bc1d79d2ea7279f43c103c704512a
EDUARDO LIMA <eduardo.eng15@hotmail.com>
2023-07-26 11:50:21 -0300

    add custom json serialization

--- src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,9 +1,19 @@
 package br.com.erudio.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import jakarta.persistence.*
+
+@JsonPropertyOrder("id","address","firstName","lastName","genero")
 data class PersonVO(
     var id: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
+
+    @field:JsonIgnore
     var address: String = "",
+
+    @field:JsonProperty("genero")
     var gender: String = ""
 )
